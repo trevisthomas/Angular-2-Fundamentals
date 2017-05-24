0d4e29320b98d2ecbea01704312c4e45c929aab1
Trevis Thomas <trevisthomas@gmail.com>
2017-05-24 13:01:55 -0500

    Validating reactive forms

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   templateUrl: 'app/user/profile.component.html',
+  styles: [`
+    em { float: right; color:#E05C65; padding-left:10px;}
+    .error input {background-color: #E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color: #999; }
+    .error :-ms-input-placeholder { color: #999; }
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup //Class scope group was needed so that we can access the controls from html
-
+  
   constructor(private authService : AuthService, private router: Router) {
 
   }
 
   ngOnInit() { //Notice, he didnt do this in the template based form, only the reactive one
-    let firstName = new FormControl(this.authService.currentUser.firstName)
+    let firstName = new FormControl(this.authService.currentUser.firstName,
+      Validators.required)
 
-    let lastName = new FormControl(this.authService.currentUser.lastName)
+    let lastName = new FormControl(this.authService.currentUser.lastName,
+      Validators.required)
 
     this.profileForm = new FormGroup({
       firstName: firstName,
@@ -28,7 +38,19 @@ export class ProfileComponent implements OnInit {
   }
 
   saveProfile(formValues){
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(['events'])
+    if( this.profileForm.valid ) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
+  }
+
+  validateLastName(){
+    return this.profileForm.controls.lastName.valid ||
+    this.profileForm.controls.lastName.untouched
+  }
+
+  validateFirstName(){
+    return this.profileForm.controls.firstName.valid ||
+    this.profileForm.controls.firstName.untouched
   }
 }
