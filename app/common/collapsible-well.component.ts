457a6638657c9a770c298400455c965ee7420f12
Trevis Thomas <trevisthomas@gmail.com>
2017-05-31 13:49:42 -0500

    Module 9 : Multiple Slot Content Projection

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -4,11 +4,13 @@ import { Component, Input } from '@angular/core'
     selector: 'collapsible-well',
     template: `
         <div (click)="toggleContent()" class="well pointable">
-            <h4 class="well-title">{{title}}</h4>
-            <ng-content *ngIf="visible"></ng-content>
+            <h4 class="well-title">
+                <ng-content select="[well-title]"></ng-content>    
+            </h4>
+            <ng-content *ngIf="visible" select="[well-body]"></ng-content>
         </div>
     `
-    
+    // Trevis, note that the 'select=' element can also take '.' and '#' style selectors
 })
 
 export class CollapsibleWell {
