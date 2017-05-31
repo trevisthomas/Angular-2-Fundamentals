0bf3c9206235b5e3668890430b31e9bf77959067
Trevis Thomas <trevisthomas@gmail.com>
2017-05-31 11:04:58 -0500

    Module 9: Content projection

--- app/app.module.ts
+++ app/app.module.ts
@@ -18,6 +18,7 @@ import {
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service'
+import { CollapsibleWell } from './common/collapsible-well.component'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
 import { AuthService } from './user/auth.service'
@@ -38,7 +39,8 @@ import { AuthService } from './user/auth.service'
         CreateEventComponent,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponent
+        SessionListComponent,
+        CollapsibleWell
     ],
     providers: [
         EventService, 
---
+++ app/common/collapsible-well.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core'
+
+@Component({
+    selector: 'collapsible-well',
+    template: `
+        <div (click)="toggleContent()" class="well pointable">
+            <h4 class="well-title">{{title}}</h4>
+            <ng-content *ngIf="visible"></ng-content>
+        </div>
+    `
+    
+})
+
+export class CollapsibleWell {
+
+    @Input() title: string
+    visible: boolean = true;
+
+    toggleContent() {
+        this.visible = !this.visible
+    }
+    
+}
