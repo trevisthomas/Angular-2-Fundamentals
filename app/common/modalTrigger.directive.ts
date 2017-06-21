53c42ac4ccfd4baa79a10a3600e634989f0f9c2e
Trevis Thomas <trevisthomas@gmail.com>
2017-06-21 11:46:49 -0500

    Module 12: “Binding an ID” Binding a variable html element id into your custom component.

--- app/common/modalTrigger.directive.ts
+++ app/common/modalTrigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Inject, ElementRef} from '@angular/core'
+import { Directive, OnInit, Inject, ElementRef, Input} from '@angular/core'
 import { JQ_TOKEN } from "./jQuery.service"
 
 @Directive({
@@ -7,6 +7,8 @@ import { JQ_TOKEN } from "./jQuery.service"
 
 export class ModalTriggerDirective implements OnInit {
     private nativeElement: HTMLElement
+    //Aliasing because dashes arent alowed in variable names
+    @Input('modal-trigger') elementId: string 
 
     constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $: any){
         this.nativeElement = ref.nativeElement
@@ -15,7 +17,12 @@ export class ModalTriggerDirective implements OnInit {
     ngOnInit() {
         this.nativeElement.addEventListener('click', e=> {
             //The object passed into modal are settings being passed to JQuery
-            this.$('#simple-modal').modal({})
+            
+            //Or the old busted way, this works too
+            // this.$('#'+this.elementId).modal({})
+
+            //An "es6 interpolation string"
+            this.$(`#${this.elementId}`).modal({})
         })
     }
 }
