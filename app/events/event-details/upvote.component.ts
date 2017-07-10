d07ce2fe82ca5da90a622cd302257efba9905b28
Trevis Thomas <trevisthomas@gmail.com>
2017-07-10 12:00:20 -0500

    Module 13: More components an custom validators - binding a style to a function using @Input setter

--- app/events/event-details/upvote.component.ts
+++ app/events/event-details/upvote.component.ts
@@ -10,8 +10,7 @@ import { ISession } from '../shared/index'
         <div class="votingWidgetContainer pointable" (click)="onClick()">
             <div class="well votingWidget">
                 <div class="votingButton">
-                    <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-                    <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+                    <i class="glyphicon glyphicon-heart" [style.color]=iconColor></i>
                 </div>
                 <div class="badge badge-inverse votingCount">
                     <div>{{count}}</div>
@@ -24,8 +23,12 @@ import { ISession } from '../shared/index'
 
 export class UpvoteComponent {
     @Input() count: number;
-    @Input() voted: boolean;
+    @Input() set voted(val){
+        this.iconColor = val ? 'red' : 'white';
+    }
+    
     @Output() vote = new EventEmitter();
+    iconColor: string;
 
     onClick() {
         this.vote.emit()
