beb56bc8643225647970464be1775569f0749ec0
Trevis Thomas <trevisthomas@gmail.com>
2017-08-23 17:22:05 -0500

    Module 15 - First karma tests

---
+++ app/test.spec.ts
@@ -0,0 +1,11 @@
+describe('First tests', () => {
+    it('should be true', () => {
+        expect(true).toBe(true)
+    })
+})
+
+describe('Second testt', () => {
+    it('should be false', () => {
+        expect(false).toBe(false)
+    })
+})
